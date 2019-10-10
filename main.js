db6ed8074aecc654d2ae2c68f899a47780dfb287
Karpenko Nataliia <Netaly79@gmail.com>
2019-10-10 19:02:31 +0300

    style

--- main.js
+++ main.js
@@ -9,35 +9,39 @@ let $stopButton=$('#stop');
 let $clickArea=$("#canvas");
 let score=document.getElementById("score");
 
+var canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
 $startButton.on("click", function (){
-  elements=[];
-  newRect();
-  animate();
+    elements=[];
+    hits=0;
+    score.innerText=hits;
+    newRect();
+    animate();
 });
 
-$stopButton.on("click", function (){
-  
-  cancelAnimationFrame(animation);
-  clearTimeout(crR);
-  clear();
+$stopButton.on("click", function (){  
+    cancelAnimationFrame(animation);
+    clearTimeout(crR);
+    clear();
 });
 
 $clickArea.on("click", function (event){
   for (let i=0; i<elements.length;i++)
   {
-    //console.log(`my ${elements[i].x},${elements[i].y}   click ${event.clientX}, ${event.clientY}`);
-    if ((elements[i].x<event.clientX && elements[i].x+30 > event.clientX ) && (event.clientY < elements[i].y+30 && event.clientY > elements[i].y))
-  {
-    elements[i].y=500; 
-hits++;
-score.innerText=hits;
-  }
+    if ((elements[i].x<event.clientX-canvas.offsetLeft && elements[i].x+30 > event.clientX-canvas.offsetLeft ) 
+    && (event.clientY-canvas.offsetTop < elements[i].y+30 && event.clientY-canvas.offsetTop > elements[i].y))
+    {
+      elements[i].y=500; 
+      hits++;
+      score.innerText=hits;
+    }
   }
 });
 
 class Rect {
   constructor(){
-    this.x=Math.round(Math.random()*640);
+    this.x=Math.round(Math.random()*630);
     this.y=0;
     this.w=20;
     this.h=20;
@@ -57,7 +61,6 @@ let newRect=function()
   el=new Rect();
   elements.push(el);
   crR=setTimeout(() => newRect(), 2000);
-  //console.log (elements.length);
 }
 
 let moveRect=function(){
@@ -66,32 +69,29 @@ let moveRect=function(){
     elements[i].y+=elements[i].speed;
   }
 }
+
 let delRect=function(){
   for (let i=0; i<elements.length; i++)
   {
     if (elements[i].y>=480)
-    elements.splice(i,1);
+      elements.splice(i,1);
   }
 }
+
 let clear=function(){
   ctx.clearRect(0,0,640,480);
   elements=[];
-  hits=0;
-  score.innerText=hits;
 }
+
 function animate() {
-  var canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+  
   ctx.clearRect(0,0,640,480); // clear canvas
 
-moveRect();
-delRect();
+  moveRect();
+  delRect();
 
-for (let i=0; i<elements.length; i++)
- elements[i].Draw(ctx);
+  for (let i=0; i<elements.length; i++)
+    elements[i].Draw(ctx);
 
- animation= requestAnimationFrame(animate);
+  animation= requestAnimationFrame(animate);
 }
-
-
-//document.body.onload = animate;
